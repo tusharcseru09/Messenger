7d85c23d625bbf2d3ded35510b04dab4fb2017e3
tusharcseru09 <tushar.cseru09@gmail.com>
2017-10-19 08:49:01 +0600

    Rest client !

--- src/main/java/org/koushik/javabrains/messenger/restClient/RestApiClient.java
+++ src/main/java/org/koushik/javabrains/messenger/restClient/RestApiClient.java
@@ -11,42 +11,53 @@ import org.koushik.javabrains.messenger.model.Message;
 
 public class RestApiClient {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		
 		Client client = ClientBuilder.newClient();
 		
-		/* Step 01
-		 * Response response = client.target("http://localhost:8989/messenger/webapi/messages/1").request().get();
-		 * Message msg = response.readEntity(Message.class);
-		 * System.out.println(msg.getMessage());
-		 **/
+/*		
+// Step 01
+		  Response response = client.target("http://localhost:8989/Messenger/webapi/messages/1").request().get();
+		  Message msg = response.readEntity(Message.class);
+		  System.out.println(msg.getMessage());
 		
-		/* Step 02
-		 * WebTarget target = client.target("http://localhost:8989/messenger/webapi/messages/1");
-		 * Builder builder = target.request();
-		 * Response response = builder.get();
-		 * Message msg = response.readEntity(Message.class);
-		 * System.out.println(msg.getMessage());
-		 **/
+*/
+
+
+/*	
+// Step 02
+		  WebTarget target = client.target("http://localhost:8989/Messenger/webapi/messages/1");
+		  Builder builder = target.request();
+		  Response response = builder.get();
+		  Message msg = response.readEntity(Message.class);
+		  System.out.println(msg.getMessage());
 		
-		/*	Step 03
-		 * 	Message msg = client.target("http://localhost:8989/messenger/webapi/messages/2")
-		 * .request(MediaType.APPLICATION_JSON)
-		 * .get(Message.class);
-		 * System.out.println(msg.getMessage());
-		 * 
-		 */
+*/	
+
+
+/*		
+// Step 03
+		  Message msg = client.target("http://localhost:8989/Messenger/webapi/messages/2")
+		  .request(MediaType.APPLICATION_JSON)
+		  .get(Message.class);
+		  System.out.println(msg.getMessage());
+		  
+*/		 
+	
+
+/*
+// Step 04
+		 	String msg = client.target("http://localhost:8989/Messenger/webapi/messages/2")
+		  	.request(MediaType.APPLICATION_JSON)
+		  	.get(String.class);
+		  	System.out.println(msg);
+		  
+*/		 
 		
-		/*	Step 04
-		 *  String msg = client.target("http://localhost:8989/messenger/webapi/messages/2")
-		 * 	.request(MediaType.APPLICATION_JSON)
-		 * 	.get(String.class);
-		 * 	System.out.println(msg);
-		 * 
-		 */
 		
-		WebTarget baseTarget = client.target("http://localhost:8989/messenger/webapi/");
+	
+		WebTarget baseTarget = client.target("http://localhost:8989/Messenger/webapi/");
 		WebTarget messageTarget = baseTarget.path("messages");
 		WebTarget singleMessageTarget = messageTarget.path("{messageId}");
 		
@@ -64,7 +75,7 @@ public class RestApiClient {
 		System.out.println(message1.getMessage());
 		System.out.println(message2.getMessage());
 		
-		
+	
 		
 	}
 }
