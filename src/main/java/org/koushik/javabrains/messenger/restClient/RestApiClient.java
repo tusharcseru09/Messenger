e3e5951c8c252d48a2add5196b9a31efd4cdb532
tusharcseru09 <tushar.cseru09@gmail.com>
2017-10-21 14:19:44 +0600

    Rest Client with Post Put and Get example.

--- src/main/java/org/koushik/javabrains/messenger/restClient/RestApiClient.java
+++ src/main/java/org/koushik/javabrains/messenger/restClient/RestApiClient.java
@@ -57,34 +57,69 @@ public class RestApiClient {
 */		 
 		
 		
-	
+/*
+ * POST Message
+ * 	
+ */
 		WebTarget baseTarget = client.target("http://localhost:8989/Messenger/webapi/");
 		WebTarget messageTarget = baseTarget.path("messages");
 		WebTarget singleMessageTarget = messageTarget.path("{messageId}");
 		
-		Message message1 = singleMessageTarget
-				.resolveTemplate("messageId", "1")
-				.request(MediaType.APPLICATION_JSON)
-				.get(Message.class);
+		//Message message1 = singleMessageTarget
+		//		.resolveTemplate("messageId", "1")
+		//		.request(MediaType.APPLICATION_JSON)
+		//		.get(Message.class);
 		
-		Message message2 = singleMessageTarget
-				.resolveTemplate("messageId", "2")
-				.request(MediaType.APPLICATION_JSON)
-				.get(Message.class);
+		//Message message2 = singleMessageTarget
+		//		.resolveTemplate("messageId", "2")
+		//		.request(MediaType.APPLICATION_JSON)
+		//		.get(Message.class);
 		
-/*
-		System.out.println(message1.getMessage());
-		System.out.println(message2.getMessage());
+		//System.out.println(message1.getMessage());
+		//System.out.println(message2.getMessage());
 		
-*/		
-		Message msg = new Message(13,"Posting Post Message.","Tushar");
+	
+		Message msg = new Message(13,"This is a simple new  message with id " + 13,"Tushar");
 		Response postResponse = messageTarget
 		.request()
 		.post(Entity.json(msg));
+		
+		System.out.println("\nPOST RESPONSE: \n" + postResponse);
 		if (postResponse.getStatus()!= 201){
-			System.out.println("Error");
+			System.out.println("Post Error!");
+		}else {
+			System.out.println("Message post is successfully done.");
+			System.out.println(postResponse.readEntity(Message.class).getMessage());
 		}
-		System.out.println(postResponse.readEntity(Message.class).getMessage());
+		
+/*
+ * PUT Message
+ * 		
+ */
+		
+		
+		Message putMsg = new Message(13,"This is a simple message updated. Msg id "+ 13,"Tushar");
+		Response putResponse = singleMessageTarget
+				.resolveTemplate("messageId", "13")
+				.request()
+				.put(Entity.json(putMsg));	
+		
+		System.out.println("\nPUT RESPONSE: \n"+putResponse);
+		if (putResponse.getStatus()!= 200){
+			System.out.println("Update Error!");
+		}else {
+			System.out.println("Message update is successfully done.");
+			System.out.println(putResponse.readEntity(Message.class).getMessage());
+		}
+		
+		
+		Response response = singleMessageTarget
+				.resolveTemplate("messageId", "13")
+				.request(MediaType.APPLICATION_JSON)
+				.get();
+		System.out.println("\nGET RESPONSE: ");
+		System.out.println(response);
+		System.out.println(response.readEntity(Message.class).getMessage());
 		
 	}
 }
